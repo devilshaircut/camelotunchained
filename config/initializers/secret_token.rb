bc0d34dcbf188e3576d8bcf0ee0942c874890570
Jason B <devilshaircut@gmail.com>
2014-02-12 18:41:11 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Camelotunchained::Application.config.secret_key_base = 'ca23588a8019c3fcb14e617fd6e4bb8c1a2ddd5500ea361a28e756925fe3beb99e4ce5971507fdbb0491b58676fcb94e44c45a84944ad15b3f59a26fca635ab7'
