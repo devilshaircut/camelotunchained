7053093f6cb858e9e97b100b6d9334c8ac810a5b
Jason B <devilshaircut@gmail.com>
2014-02-12 19:56:25 -0500

    Add in a data table of sample data.

--- app/assets/javascripts/camelotunchained_app/controllers/homeController.js
+++ app/assets/javascripts/camelotunchained_app/controllers/homeController.js
@@ -4,15 +4,42 @@ CamelotUnchained.controller("homeController", ["$scope", "httpService", "$route"
   var apiEndpoint = '/sample.json';
 
   // This is the callback function that executes if the HTTP requests returns successfully.
-  var getSampleSuccess = function(payload, status) {
-    $scope.sample = payload;
+  var getTakedownsSuccess = function(payload, status) {
+    $scope.takedowns = payload;
+    $scope.players = {};
+    for(var i=0; i < payload.length; i++) {
+      var interaction = payload[i];
+      if ($scope.players[interaction["victim"]["name"]]) {
+        $scope.players[interaction["victim"]["name"]]["deaths"] += 1;
+      }
+      else {
+        $scope.players[interaction["victim"]["name"]] = {
+          "name": interaction["victim"]["name"],
+          "faction": interaction["victim"]["faction"],
+          "kills": 0,
+          "deaths": 1
+        }
+      }
+      if ($scope.players[interaction["killer"]["name"]]) {
+        $scope.players[interaction["killer"]["name"]]["kills"] += 1;
+      }
+      else {
+        $scope.players[interaction["killer"]["name"]] = {
+          "name": interaction["killer"]["name"],
+          "faction": interaction["killer"]["faction"],
+          "kills": 1,
+          "deaths": 0
+        }
+      }
+    }
+    console.log($scope.players);
   };
 
   // This is the callback function that executes if the HTTP requests returns unsuccessfully.
-  var getSampleFailure = function(payload, status) {};
+  var getTakedownsFailure = function(payload, status) {};
 
   // Initiate the HTTP request.
-  httpService.getApiEndpoint(apiEndpoint).success(getSampleSuccess);
+  httpService.getApiEndpoint(apiEndpoint).success(getTakedownsSuccess);
 
 }]);
 
